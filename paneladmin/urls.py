f074f890fd3ec4b0501c4fc8c70f444636d477e7
Juan Daniel Gaviria Gomez <daniel@MacBook-Air-de-Juan.local>
2025-01-25 15:41:33 -0500

    Feat: Create HTML templates for panel

--- paneladmin/urls.py
+++ paneladmin/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from paneladmin import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('login/', views.login),
-    path('panelindex/', views.panelindex),
+    path('login/', views.loginpanel, name='loginpanel'),
+    path('panelindex/', views.panelindex, name='panelindex'),
+    path('logout/', views.signout, name='logout'),
 ]
--- paneladmin/views.py
+++ paneladmin/views.py
@@ -1,10 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
-def login(request):
-    return render(request, 'login.html')
+def loginpanel(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'login.html', {
+                'error' : 'Nombre de usuario o contraseña incorrectos y/o no existen'
+            })
+        else:
+            user.save()
+            login(request, user)
+            return redirect('panelindex')
 
+@login_required
 def panelindex(request):
     return render(request, 'panelindex.html')
 
+def signout(request):
+    logout(request)
+    return redirect('loginpanel')
+    
